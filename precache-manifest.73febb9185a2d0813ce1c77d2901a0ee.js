06a126967beaf5f91f6e71d3ded8c419d7b4af96
AndresSp <AndresSp@users.noreply.github.com>
2020-06-02 06:21:31 +0000

    Deploying to gh-pages from  @ da3a5c29914e847eba11e5f097abf67aa01c89c2 🚀

---
+++ precache-manifest.73febb9185a2d0813ce1c77d2901a0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57671f2187668ee9dfcadc368b47d9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede837659efe3728060c",
+    "url": "/static/css/main.788146c5.chunk.css"
+  },
+  {
+    "revision": "c0ebfec394a2d5effcf5",
+    "url": "/static/js/2.e837c8ba.chunk.js"
+  },
+  {
+    "revision": "14b6951eb3599e7c836b2179fd9b2b8b",
+    "url": "/static/js/2.e837c8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede837659efe3728060c",
+    "url": "/static/js/main.2b34ba68.chunk.js"
+  },
+  {
+    "revision": "416bc22f6a2cd765bf6c",
+    "url": "/static/js/runtime-main.95004f6e.js"
+  }
+]);
